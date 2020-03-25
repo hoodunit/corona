1f784e0839eff8e31408e72e8f80f8518116b300
Nicholas Kariniemi <nicholas.kariniemi@reaktor.com>
2020-03-25 21:07:42 +0200

    TypeScript hello world client app

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const webpack = require("webpack")
+const path = require("path")
+
+const config = {
+  mode: "production",
+  devtool: "inline-source-map",
+  target: "web",
+  resolve: {
+    extensions: [".js", ".mjs"],
+  },
+  entry: [path.join(__dirname, "./output/app.js")],
+  output: {
+    filename: "app.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ["source-map-loader"],
+        enforce: "pre",
+        exclude: [path.resolve(__dirname, "node_modules/")],
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg|ico)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      },
+    ],
+  }
+}
+
+module.exports = [config]
