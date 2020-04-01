a81881cde22b757fda2dd446ee5da52871570864
Nicholas Kariniemi <nicholas.kariniemi@reaktor.com>
2020-04-01 19:48:53 +0300

    Fix: fill empty data points with prev day's values

--- src/data.ts
+++ src/data.ts
@@ -1,9 +1,12 @@
-import { array, either } from "fp-ts"
+import { array, either, option, ord, record } from "fp-ts"
+import { pipe } from "fp-ts/es6/pipeable"
+import { ordNumber } from "fp-ts/lib/Ord"
 import * as t from "io-ts"
 import * as IoTsReporter from "io-ts-reporters"
 import { DateFromString, NumberFromString } from "./codecs"
 import Papa = require("papaparse")
 import * as fetchPonyfill from "fetch-ponyfill"
+import * as DateFns from "date-fns/fp"
 
 const fetchPony = fetchPonyfill({}).fetch
 
@@ -35,11 +38,66 @@ export const getData = async (): Promise<CoronaData> => {
   const countries = await getCountries()
   const states = await getStates()
   const counties = await getCounties()
-  return {
+  const fullData = {
     ...states,
     ...countries,
     ...counties
   }
+  const sorted = record.map(sortByDate)(fullData)
+  const filled = record.map(fillEmptyEntries)(sorted)
+  validateEntries(filled)
+  return filled
+}
+
+const sortByDate = (entries: Array<DateEntry>): Array<DateEntry> => {
+  const byDate = pipe(
+    ordNumber,
+    ord.contramap((d: DateEntry) => d.date.getTime())
+  )
+  return array.sort(byDate)(entries)
+}
+
+const fillEmptyEntries = (entries: Array<DateEntry>): Array<DateEntry> => {
+  const fillEntries = array.reduce([], (cur: Array<DateEntry>, next: DateEntry) => {
+    const prev = cur[cur.length - 1]
+    if (!prev) {
+      return array.snoc(cur, next)
+    }
+    const dayDiff = DateFns.differenceInDays(prev.date, next.date)
+    if (dayDiff <= 1) {
+      return array.snoc(cur, next)
+    }
+    const missing: Array<DateEntry> = pipe(
+      array.range(1, dayDiff - 1),
+      array.map(days => {
+        return {
+          date: DateFns.addDays(days)(prev.date),
+          confirmed: prev.confirmed,
+          deaths: prev.deaths,
+          recovered: prev.recovered
+        }
+      })
+    )
+    return cur.concat(missing)
+  })
+  return fillEntries(entries)
+}
+
+const validateEntries = (data: CoronaData) => {
+  record.mapWithIndex(validateEntry)(data)
+}
+
+const validateEntry = (key: string, vals: Array<DateEntry>): void => {
+  let prev: DateEntry | undefined = undefined
+  vals.forEach((next, index) => {
+    if (prev && DateFns.isAfter(next.date, prev.date)) {
+      throw new Error(`At ${key}[${index}]: expected ${prev.date} to be before ${next.date}`)
+    }
+    if (prev && DateFns.differenceInDays(prev.date, next.date) > 1) {
+      throw new Error(`At ${key}[${index}]: expected ${prev.date} to be within one day of ${next.date}`)
+    }
+    prev = next
+  })
 }
 
 const getCountries = (): Promise<CoronaData> => {
